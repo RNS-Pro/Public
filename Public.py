2fcf42414c15ae68a025ee0210d3ad9d92a6324c
Mr-SOMAIL <105285038+RNS-Pro@users.noreply.github.com>
2022-06-13 22:14:02 +0500

    Create Public.py

---
+++ Public.py
@@ -0,0 +1,35 @@
+import os, platform,time
+
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+from time import sleep
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from crak import expetmysite
+
+    print("\n\n")
+
+    time.sleep(1)
+
+    expetmysite()
+
+elif bit == '32bit':
+
+    from crak import expetmysite
+
+    print("\n Congratulations! Your device supported!\n")
+
+    time.sleep(3)
+
+    expetmysite()
