7c6961eee2fa833b7f15951ae251ddc1c6d282ca
Mr-SOMAIL <105285038+RNS-Pro@users.noreply.github.com>
2022-06-14 23:46:12 +0500

    Update Public.py

--- Public.py
+++ Public.py
@@ -4,8 +4,8 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from Cracker import Main
+    from crak import Main
     Main()
 elif bit == '32bit':
-    from Cracker import Main
+    from crak import Main
     Main()
