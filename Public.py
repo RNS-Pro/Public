bf0ca2047c65febc31635f7e990e4b4765f20148
Mr-SOMAIL <105285038+RNS-Pro@users.noreply.github.com>
2022-06-14 23:04:45 +0500

    Update Public.py

--- Public.py
+++ Public.py
@@ -1,35 +1,11 @@
-import os, platform,time
-
-try:
-
-   import requests
-
-except:
-
-   os.system('pip2 install requests')
-
-from time import sleep
-
+import os, platform
+os.system('git pull')
+ 
 import requests
-
 bit = platform.architecture()[0]
-
 if bit == '64bit':
-
-    from Cracker import expetmysite
-
-    print("\n\n")
-
-    time.sleep(1)
-
-    expetmysite()
-
+    from DUMP import menu
+    menu()
 elif bit == '32bit':
-
-    from Cracker import expetmysite
-
-    print("\n Congratulations! Your device supported!\n")
-
-    time.sleep(3)
-
-    expetmysite()
+    from EXTRACT import menu
+    menu()
